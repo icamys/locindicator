691780ba5272a8a4aa91162b78edc52e39dff756
icamys <prisacari.dmitrii@gmail.com>
2020-11-10 20:55:04 +0200

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 INSTALL_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y &&
-  apt-get update &&
-  apt-get install -y jq indicator-sysmonitor
+sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y
+sudo apt-get update
+sudo apt-get install -y jq indicator-sysmonitor
 
 python "${INSTALL_PATH}"/configure_indicator.py
 
