4e9b5430f17aa01fe5e8dd12be141c266239dac8
icamys <prisacari.dmitrii@gmail.com>
2020-11-12 13:56:50 +0200

    rm indicator desktop file in uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,3 +2,4 @@
 sudo apt-get remove -y jq indicator-sysmonitor
 pkill -f /usr/bin/indicator-sysmonitor
 rm "$HOME"/.indicator-sysmonitor.json
+rm "$HOME/.config/autostart/indicator-sysmonitor.desktop"
