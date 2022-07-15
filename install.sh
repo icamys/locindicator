8b9464b6524860869489f25745226c0d9f406cdb
Dmitrii Prisacari <prisacari.dmitrii@gmail.com>
2022-07-15 21:40:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y
-sudo apt-get update
-sudo apt-get install -y jq indicator-sysmonitor
+sudo apt-get install -y jq python3-psutil curl git gir1.2-appindicator3-0.1
+git clone https://github.com/fossfreedom/indicator-sysmonitor.git
+cd indicator-sysmonitor
+sudo make install
+cd ../
+rm -rf ./indicator-sysmonitor
