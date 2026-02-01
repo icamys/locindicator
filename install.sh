0dffd97c98b1e7b743bc89f28ac12a39505ea9f0
Dmitrii Prisacari <prisacari.dmitrii@gmail.com>
2026-02-01 22:13:48 +0000

    fixing the indicator installed version

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install -y jq python3-psutil curl git gir1.2-appindicator3-0.1
+sudo apt-get install -y jq python3-psutil curl git gir1.2-appindicator3-0.1 meson ninja-build
+
 git clone https://github.com/fossfreedom/indicator-sysmonitor.git
 cd indicator-sysmonitor
-sudo make install
-cd ../
+git reset --hard cc5d095 # version the locindicator was tested against
+
+# Building the appindicator according to its docs
+mkdir build
+cd build
+meson setup --prefix=/usr
+sudo meson install
+
+# Cleaning up
+cd ../../
 rm -rf ./indicator-sysmonitor
